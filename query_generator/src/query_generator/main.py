7c78b968f003fa55d4af9900eb248729165d6a14
madhurchouhan01 <madhurchouhan02@gmail.com>
2025-04-16 18:34:56 +0530

    d

--- query_generator/src/query_generator/main.py
+++ query_generator/src/query_generator/main.py
@@ -1,47 +1,77 @@
 from pydantic import BaseModel
 
-from crewai.flow.flow import Flow, listen, start
+from crewai.flow.flow import Flow, listen, start, router
 
-from crews.query_crew.generate_query_crew import QueryCrew, QueryGenCrew
+from crews.query_crew.generate_query_crew import QueryCrew, QueryGenCrew, IDUGenCrew
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class QueryState(BaseModel):
     query : str = "" 
+    history : list = []
     sql_query : str = ""
     final_response : str = ""
-
 
 class QueryFlow(Flow[QueryState]):
 
     @start()
-    def generate_sentence_count(self):
-        query = input("🤖   Please enter your query :   ")
-        self.state.query = query
-        return query
-
-    @listen(generate_sentence_count)
     def generate_query(self):
-        print("🚀   Delegating to DB agent...")
-        result = QueryCrew().crew().kickoff(inputs={"query": self.state.query})
+        # chat_history = QueryState.history
+        # history = '\n'.join(chat_history)
+        # query   = QueryState.query
+        # print(f"query inside generate_query function : {query}")
+        # print(f"chat history inside generate_query function is : {history}")
+        # print("🚀  Delegating to DB agent...")
+        user_input = input("🤖  Please enter your query (or type 'exit') :   ")
+        self.state.query = user_input
+        result = QueryCrew().crew().kickoff(inputs={"query": self.state.query, "history" : self.state.history})
 
-        print("✅   Generated Query : ", result.raw)
+        # print("✅  Generated Query : ", result.raw)
         self.state.sql_query = result.raw
 
         return self.state.sql_query
+    
+    @router(generate_query)
+    def router_func(self):
+        if 'SELECT' in self.state.sql_query:
+            return "SELECT"
+        elif 'INSERT' in self.state.sql_query or 'DELETE'  in self.state.sql_query or'UPDATE' in self.state.sql_query:
+            return "IDU"
 
-    @listen(generate_query)
-    def execute_query(self):
-        print("Executing Query...")
+    @listen("SELECT")
+    def select_query(self):
+        print("🔴🔴🔴🔴🔴Executing Query...")
         result = QueryGenCrew().crew().kickoff(inputs={"sql_query": self.state.sql_query})
         self.state.final_response = result.raw
+        self.state.history.append(f"AI Response : {self.state.final_response}")
+        return self.state.final_response
+    
+    @listen("IDU")
+    def idu_query(self):
+        print("🍒🍒🍒🍒🍒This is IDU Query...")
+        result = IDUGenCrew().crew().kickoff(inputs={"sql_query": self.state.sql_query})
+        self.state.final_response = result.raw
+        self.state.history.append(f"AI Response : {self.state.final_response}")
         return self.state.final_response
 
 if __name__ == "__main__":
-    query_flow = QueryFlow()
-    first_result = query_flow.kickoff()
 
-    print("😍 Final response from bot : ")
-    print(first_result)
+    query_state = QueryState()
+    query_flow = QueryFlow()
+    response = query_flow.kickoff()
+    # while True:
+    #     user_input = input("🤖  Please enter your query (or type 'exit') :   ")
+    #     if user_input.lower() == 'exit':
+    #         break
+    #     print(f"User query is : {user_input}")
+    #     query_state.history.append(f"user : {user_input}")
+    #     print(f"Initial history is : {query_state.history}")
+    #     query_state.query = user_input
+    #     print(f"query in state is : {query_state.query}")
+    #     response = query_flow.kickoff(inputs={"query" : user_input, "history" : QueryState.history})
+
+    # print("😍 Final response from bot : ")
+    # print(response)
--- query_generator/src/query_generator/tools/custom_tool.py
+++ query_generator/src/query_generator/tools/custom_tool.py
@@ -20,3 +20,42 @@ def query_executer(sql_query: str) -> str:
     finally:
         cursor.close()
         conn.close()
+
+
+@tool("IDU Query Executer")
+def idu_query_executer(sql_query: str) -> str:
+    """Executes the given Insertion, Deletion, or Updation SQL query and returns the result as a plain string."""
+
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="1234",
+        database="employee"
+    )
+    cursor = conn.cursor()
+    
+    try:
+        cursor.execute(sql_query)
+        
+        conn.commit()
+        
+        if sql_query.strip().upper().startswith("INSERT"):
+            return "Insertion successful!"
+        
+        elif sql_query.strip().upper().startswith("DELETE"):
+            rows_affected = cursor.rowcount
+            return f"{rows_affected} row(s) deleted."
+        
+        elif sql_query.strip().upper().startswith("UPDATE"):
+            rows_affected = cursor.rowcount
+            return f"{rows_affected} row(s) updated."
+        
+        else:
+            return "Query executed successfully, but no action needed for this type."
+        
+    except Exception as e:
+        return f"Error executing query: {e}"
+    
+    finally:
+        cursor.close()
+        conn.close()
